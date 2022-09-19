6fcc09577d76190d8881cfd667656ed9f5266ac7
YANG QIA <2013xile@gmail.com>
2022-09-19 17:53:12 +0800

    request with pagination

--- src/table/index.tsx
+++ src/table/index.tsx
@@ -1,17 +1,42 @@
 import {parseContent, TableDefinition} from '@easycrud/toolkits';
 import {Table, TableProps} from 'antd';
-import type {ColumnType} from 'antd/es/table';
+import type {ColumnType, TablePaginationConfig} from 'antd/es/table';
 import deepmerge from 'deepmerge';
 import React, {useEffect, useState} from 'react';
 
 type CrudTableProps = TableProps<Record<string, any>> &
 {
   tableDef: TableDefinition,
-  baseUrl?: string,
+  api?: string,
 }
 
+async function request(
+    api?: string,
+    pagination: TablePaginationConfig = {current: 1, pageSize: 20},
+) {
+  if (!api) {
+    return;
+  }
+
+  const {current, pageSize} = pagination;
+
+  const res = await fetch(api, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    mode: 'cors',
+    body: JSON.stringify({
+      data: {
+        page: current,
+        pageSize,
+      },
+    }),
+  });
+  const {data} = await res.json();
+  return data.data;
+};
+
 function CrudTable(props: CrudTableProps) {
-  const {tableDef, baseUrl, columns, dataSource} = props;
+  const {tableDef, api, columns, dataSource} = props;
   const table = parseContent(tableDef);
   const fields = table.columns;
   const fieldMap = fields.reduce<{
@@ -34,33 +59,18 @@ function CrudTable(props: CrudTableProps) {
   }, {});
   const mergedColumns = deepmerge(fieldMap, columnMap);
 
-  const [data, setData] = useState(dataSource || []);
+  const [data, setData] = useState(dataSource);
   useEffect(() => {
-    if (data && data.length) {
+    if (data) {
       return;
     }
-    const request = async () => {
-      // const res = await fetch(`${baseUrl}/${table.tableName}`, {
-      //   method: 'POST',
-      //   headers: {'Content-Type': 'application/json'},
-      //   mode: 'cors',
-      //   body: JSON.stringify({
-      //     data: {
-      //     },
-      //   }),
-      // });
-      // const {data} = await res.json();
-      // setData(data.data);
-      setData([{
-        id: 1,
-      }]);
-    };
-    request().catch((err)=> {
+    request(api).then((data) => setData(data)).catch((err)=> {
       console.log(err);
     });
-  }, [baseUrl, table, data]);
+  }, [api, data]);
 
   return <Table
+    onChange={(pagination) => request(api, pagination)}
     {...props}
     dataSource={data}
     columns={Object.values(mergedColumns)} />;
